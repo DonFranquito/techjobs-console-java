fcf25ef2de04321911c2f107ad07982b81744155
DonFranquito <50225179+DonFranquito@users.noreply.github.com>
2019-08-07 19:28:00 -0500

    Finished base assignment TechJobs(Console Edition)

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -58,10 +58,10 @@ public class TechJobs {
 
                 // What is their search term?
                 System.out.println("\nSearch term: ");
-                String searchTerm = in.nextLine();
+                String searchTerm = in.nextLine().toLowerCase();
 
                 if (searchField.equals("all")) {
-                    System.out.println("Search all fields not yet implemented.");
+                    printJobs(JobData.findByValue(searchTerm));
                 } else {
                     printJobs(JobData.findByColumnAndValue(searchField, searchTerm));
                 }
@@ -110,7 +110,20 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-
-        System.out.println("printJobs is not implemented yet");
+        //iterates over the size of the someJobs and then indexes into the hash map to display
+        //each jobs details
+        if (someJobs.size() > 0) {
+            for (int i = 0; i < someJobs.size(); i++) {
+                System.out.println("*****");
+                System.out.println("Position Type: " + someJobs.get(i).get("position type"));
+                System.out.println("Name: " + someJobs.get(i).get("name"));
+                System.out.println("Employer: " + someJobs.get(i).get("employer"));
+                System.out.println("Location: " + someJobs.get(i).get("location"));
+                System.out.println("Core Competency: " + someJobs.get(i).get("core competency"));
+                System.out.println("*****");
+            }
+        } else {
+            System.out.println("No Jobs found with those search terms");
+        }
     }
 }
